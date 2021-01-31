92e1c13e21e4db1a23f3e6dbbe7513def87e0ed0
Arvind Venkatesan <varvind@tamu.edu>
2021-01-31 16:06:19 -0600

    made another hot fix to database url

--- app/controllers/add_book_view_controller.rb
+++ app/controllers/add_book_view_controller.rb
@@ -1,25 +1,28 @@
 require 'pg'
 class AddBookViewController < ApplicationController
-  def create
-    conn = PG::Connection.open(:dbname => 'book_collection_development')
-    title = params[:title]
-    author = params[:author]
-    genre = params[:genre]
-    price = params[:price]
-    pub_date = params[:published_date]
-
-    begin
-      conn.exec "INSERT INTO Books Values('#{title}', '#{author}', '#{genre}', #{price}, '#{pub_date}');"
-      redirect_to '/'
-    rescue
-      flash[:notice] = "Invalid entry"
-      redirect_to '/addbook'
-    ensure
-      conn.close if conn
-    end
-  end
-  def remove 
+    def add_book 
     
-  end
+    end
+    def create
+        conn = PG::Connection.open(:dbname => 'book_collection_development')
+        title = params[:title]
+        author = params[:author]
+        genre = params[:genre]
+        price = params[:price]
+        pub_date = params[:published_date]
+
+        begin
+        conn.exec "INSERT INTO Books Values('#{title}', '#{author}', '#{genre}', #{price}, '#{pub_date}');"
+        redirect_to '/'
+        rescue
+        flash[:notice] = "Invalid entry"
+        redirect_to '/addbook'
+        ensure
+        conn.close if conn
+        end
+    end
+    def remove 
+        
+    end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,18 @@ require 'pg'
 class HomeController < ApplicationController
   def index 
     @books = []
-    puts ENV['DATABASE_URL']
+    url = "postgres://arvind2529:arvind00@localhost:5432/book_collection_development"
+    
     db_name = 'book_collection_development'
     if Rails.env == 'production'
+        url = ENV['DATABASE_URL']
         db_name = Rails.configuration.database_configuration["production"]["database"]
     end
 
+    uri = URI.parse(url)
+
     begin 
-      conn = PG.connect(:dbname => db_name)
+      conn = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
       @books = conn.exec "SELECT * From Books"
     rescue => e
         puts e.message
