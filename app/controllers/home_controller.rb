036cca4e0ff2b267885d363a72a8fb5784712ab0
Arvind Venkatesan <varvind@tamu.edu>
2021-01-31 14:35:43 -0600

    tried to fix production db name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,20 @@ require 'pg'
 class HomeController < ApplicationController
   def index 
     @books = []
+    puts 
+    puts "howdy"
     db_name = 'book_collection_development'
     if Rails.env == 'production'
-        db_name = 'book_collection_production'
+        db_name = Rails.configuration.database_configuration["production"]["database"]
     end
-    # begin 
+    begin 
       conn = PG::Connection.open(:dbname => db_name)
       @books = conn.exec "SELECT * From Books"
-    # rescue
+    rescue
       flash[:notice] = "Error in retreiving books, please check database connection"
-    # ensure
+    ensure
       conn.close if conn
-    # end
+    end
     
   end
 end
