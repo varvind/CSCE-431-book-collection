4bb64bb32693177b4479cc2be8477444b9f15fe9
Arvind Venkatesan <varvind@tamu.edu>
2021-01-31 13:01:47 -0600

    made small change to routes to fix render

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' ,to: 'home#index'
+  root 'home#index'
   get '/addbook', to: 'add_book_view#addbook'
   post '/addbook' => 'add_book_view#create'
   get '/remove/:title'=> 'remove#remove'
