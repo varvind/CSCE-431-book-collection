e47d9b92501ad5633625dfa581cf44b3c11d14ae
Arvind Venkatesan <varvind@tamu.edu>
2021-01-31 15:43:15 -0600

    made another hot fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,12 @@ class HomeController < ApplicationController
     @books = []
     puts ENV['DATABASE_URL']
     db_name = 'book_collection_development'
-    conn = conn = PG::Connection.open(:dbname => db_name)
     if Rails.env == 'production'
-        conn = PG.connect(ENV['DATABASE_URL'])
+        db_name = Rails.configuration.database_configuration["production"]["database"]
     end
 
     begin 
-      
+      conn = PG.connect(:dbname => db_name)
       @books = conn.exec "SELECT * From Books"
     rescue => e
         puts e.message
