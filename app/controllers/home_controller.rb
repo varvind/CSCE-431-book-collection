f406b99396350b56e82d58123dd46c6757a79940
Arvind Venkatesan <varvind@tamu.edu>
2021-01-31 15:01:12 -0600

    added username

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,8 @@ class HomeController < ApplicationController
         db_name = Rails.configuration.database_configuration["production"]["database"]
     end
 
-    puts db_name
     begin 
-      conn = PG::Connection.open(:dbname => db_name)
+      conn = PG::Connection.open(:dbname => db_name, :user => "arvind2529")
       @books = conn.exec "SELECT * From Books"
     rescue
       flash[:notice] = "Error in retreiving books, please check database connection"
